e569d91d58ac0f2773ddee43f0a3737ff256bbf0
1alberto994 <alby94ct@gmail.com>
2023-01-23 15:45:45 +0100

    esercizio completato

--- script.js
+++ script.js
@@ -3,30 +3,73 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-         slides : 
-            {
-                    image: ['https://picsum.photos/id/0/200/',
-                            'https://picsum.photos/id/1/200/',
-                            'https://picsum.photos/id/2/200/',
-                            'https://picsum.photos/id/3/200/',
-                            'https://picsum.photos/id/4/200/',
-                            'https://picsum.photos/id/5/200/'
-                ]
-                   
-                }
+         
+            
+               image: ['https://picsum.photos/id/0/200/',
+                        'https://picsum.photos/id/1/200/',
+                        'https://picsum.photos/id/2/200/',
+                        'https://picsum.photos/id/3/200/',
+                        'https://picsum.photos/id/4/200/',
+                        'https://picsum.photos/id/5/200/'
+                     ],
+               text:[
+                  "QUESTO COMPUTER è BELLISSIMO",
+                  "QUESTO COMPUTER è  ECONOMICO",
+                  "QUESTO COMPUTER è  STUPENDO",
+                  "QUESTO COMPUTER è  SULLA MEDIA",
+                  "QUESTO COMPUTER è  TOP",
+                  "QUESTO COMPUTER è  IL FUTURO"
+               ],
+                        
+                     activeImage:0,
+                     activeText:0    
+            
+                 
                     
             
       }
       
     },
-    activeSlides:0,
+    
     methods:{
         
      avantiImg(){
-        activeSlides++
+      
+        if(this.activeImage==this.image.length -1){
+         this.activeImage=0
+        }
+        else{
+         this.activeImage++
+        }
      },
      indietroImg(){
-        activeSlides--
-     }
+      
+      if(this.activeImage==0){
+         this.activeImage=this.image.length -1
+        }
+        else{
+         this.activeImage--
+        }
+        
+     },
+     avantiText(){
+      
+      if(this.activeText==this.text.length -1){
+       this.activeText=0
+      }
+      else{
+       this.activeText++
+      }
+   },
+   indietroText(){
+    
+    if(this.activeText==0){
+       this.activeText=this.text.length -1
+      }
+      else{
+       this.activeText--
+      }
+      
+   }
     }
   }).mount('#app')
